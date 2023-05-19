767e438e122341d3156c9a63fc82ad433d6b024e
Lucas Tamir Faria <lucastamir@gmail.com>
2023-05-19 09:45:34 -0300

    :sparkles: Setting API folder

---
+++ pages/api/index.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(request: NextApiRequest, response: NextApiResponse) {
+  response.status(200).json({message: "Hello, world!", headers: request.headers})
+}
