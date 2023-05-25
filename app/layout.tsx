43c4a0202e02635d5047eec44b5e207459ef1540
Lucas Tamir Faria <lucastamir@gmail.com>
2023-05-25 18:11:04 -0300

    :bug: Fixing a render bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
 }
