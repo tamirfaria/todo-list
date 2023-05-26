2e3e9ce0c29b55d909312c49965bf2ff628a5538
Lucas Tamir Faria <lucastamir@gmail.com>
2023-05-26 09:47:04 -0300

    :art: Adding title in head of application

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>to do list</title>
+      </head>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
