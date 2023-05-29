fe8a61c387f4fa1138e05892d607747b955f031d
Lucas Tamir Faria <lucastamir@gmail.com>
2023-05-29 15:14:59 -0300

    :hammer: Removing pagination logic from client layer

--- src/client/repository/todo.ts
+++ src/client/repository/todo.ts
@@ -3,7 +3,7 @@ import { validatedResponse } from "@utils/validatedResponse";
 export interface Todo {
   id: string;
   content: string;
-  date: string;
+  date: Date;
   done: boolean;
 }
 
@@ -22,20 +22,14 @@ async function get({
   page,
   limit,
 }: TodoRepositoryGetParams): Promise<TodoRepositoryGetResponse> {
-  const res = await fetch("/api/todos");
+  const res = await fetch(`/api/todos?page=${page}&limit=${limit}`);
   const data = await res.json();
   const convertedData = validatedResponse(data);
 
-  const totalData = convertedData.todos.length;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const paginatedData = convertedData.todos.slice(startIndex, endIndex);
-  const totalPages = Math.ceil(totalData / limit);
-
   return {
-    todos: paginatedData,
-    total: totalData,
-    pages: totalPages,
+    todos: convertedData.todos,
+    total: convertedData.total,
+    pages: convertedData.pages,
   };
 }
 
