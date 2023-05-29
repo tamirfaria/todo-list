1953c886c1bfd2f96bdbcbea425971a7293773ba
Lucas Tamir Faria <lucastamir@gmail.com>
2023-05-29 15:11:36 -0300

    :bug: Fixing formatedDate utils code

--- src/utils/formatedDate.ts
+++ src/utils/formatedDate.ts
@@ -1,4 +1,5 @@
-export const formatedDate = (date: string) => {
-  const dateToArray = date.split("-");
+export const formatedDate = (date: Date) => {
+  const dateToString = String(date).split("T")[0];
+  const dateToArray = dateToString.split("-");
   return `${dateToArray[2]}/${dateToArray[1]}/${dateToArray[0]}`;
 };
