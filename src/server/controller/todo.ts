e4a05abc12396d3a5e746bde72016f1feb11221a
Lucas Tamir Faria <lucastamir@gmail.com>
2023-05-29 15:16:05 -0300

    :hammer: Implementing pagination logic at the server layer

--- src/server/controller/todo.ts
+++ src/server/controller/todo.ts
@@ -1,10 +1,34 @@
-import { read } from "@db-crud-todo";
+import { todoRepository } from "@server/repository/todo";
 import { NextApiRequest, NextApiResponse } from "next";
 
-function get(_: NextApiRequest, res: NextApiResponse) {
-  const ALL_TODOS = read();
+function get(req: NextApiRequest, res: NextApiResponse) {
+  const page = Number(req.query.page);
+  const limit = Number(req.query.limit);
+
+  if (req.query.page && isNaN(page)) {
+    res.status(400).json({
+      error: {
+        message: "page must be a number",
+      },
+    });
+    return;
+  }
+
+  if (req.query.limit && isNaN(limit)) {
+    res.status(400).json({
+      error: {
+        message: "limit must be a number",
+      },
+    });
+    return;
+  }
+
+  const response = todoRepository.get({ page, limit });
+
   res.status(200).json({
-    todos: ALL_TODOS,
+    pages: response.pages,
+    total: response.total,
+    todos: response.todos,
   });
 }
 
---
+++ src/server/repository/todo.ts
@@ -0,0 +1,40 @@
+import { read } from "@db-crud-todo";
+
+interface Todo {
+  id: string;
+  content: string;
+  date: Date;
+  done: boolean;
+}
+
+interface TodoRepositoryGetParams {
+  page?: number;
+  limit?: number;
+}
+
+interface TodoRepositoryGetResponse {
+  pages: number;
+  total: number;
+  todos: Todo[];
+}
+
+function get({
+  page,
+  limit,
+}: TodoRepositoryGetParams): TodoRepositoryGetResponse {
+  const currentPage = page || 1;
+  const currentLimit = limit || 1;
+  const ALL_TODOS = read();
+
+  const startIndex = (currentPage - 1) * currentLimit;
+  const endIndex = currentPage * currentLimit;
+  const paginatedTodos = ALL_TODOS.slice(startIndex, endIndex);
+  const totalTodos = ALL_TODOS.length;
+  const totalPages = Math.ceil(totalTodos / currentLimit);
+
+  return { pages: totalPages, total: totalTodos, todos: paginatedTodos };
+}
+
+export const todoRepository = {
+  get,
+};
