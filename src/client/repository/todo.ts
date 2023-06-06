7461092e458448a211f4960c98a4f0ffbaa53224
Lucas Tamir Faria <lucastamir@gmail.com>
2023-06-06 10:27:47 -0300

    :sparkles: Creating a post function in client repository

--- src/client/repository/todo.ts
+++ src/client/repository/todo.ts
@@ -1,4 +1,4 @@
-import { Todo } from "@client/model/todo";
+import { ServerResponseSchema, Todo } from "@client/schema";
 import { validatedResponse } from "@utils/validatedResponse";
 
 interface TodoRepositoryGetParams {
@@ -29,6 +29,30 @@ async function get({
   };
 }
 
+async function createByContent(content: string): Promise<Todo> {
+  const response = await fetch("api/todos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ content }),
+  });
+
+  if (response.ok) {
+    const serverResponse = await response.json();
+    const serverResponseParsed = ServerResponseSchema.safeParse(serverResponse);
+
+    if (!serverResponseParsed.success) {
+      throw new Error("Failed to create a new TODO...");
+    }
+
+    const todo = serverResponseParsed.data.todo;
+    return todo;
+  }
+  throw new Error("Failed to create a new TODO...");
+}
+
 export const todoRepository = {
   get,
+  createByContent,
 };
