64bb3603f9cdabf21e6c282413e576f60dba28c6
Lucas Tamir Faria <lucastamir@gmail.com>
2023-06-08 17:18:12 -0300

    :wrench: Changing API structures of delete and put methods

--- pages/api/todos/[id]/index.ts
+++ pages/api/todos/[id]/index.ts
@@ -2,6 +2,11 @@ import { todoController } from "@server/controller/todo";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "DELETE") {
+    todoController.deleteTodoById(req, res);
+    return;
+  }
+
   if (req.method === "PUT") {
     todoController.toggleDone(req, res);
     return;
